2c8b19c8ee8a76bac84b0a46ecd5bbcf5f531d24
Amish Bharti <47895005+amish1999@users.noreply.github.com>
2020-10-01 00:05:14 +0530

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -8,3 +8,27 @@ def index(request):
     context={'latest_question_list':latest_question_list}
     return render(request, 'polls/index.html',context)
 
+def detail(request,question_id):
+    question=get_object_or_404(Question,pk=question_id)
+    context={'question':question}
+    return render(request,'polls/detail.html',context)
+
+def result(request,question_id):
+    question=get_object_or_404(Question,pk=question_id)
+    context={'question':question}
+    return render(request,'polls/results.html',context)
+
+def vote(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        print(request.POST['choice'])
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',{
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
